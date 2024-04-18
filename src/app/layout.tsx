ba4f3dab4ce847ed6346ebad4c80d09ea1af89ca
freebird920 <freebird_han@naver.com>
2024-04-18 14:40:12 +0900

    네비게이션 바 스타일 수정 및 레이아웃 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import CompNavbar from "@/components/navbar/CompNavbar";
 
-
-
 export const metadata: Metadata = {
   title: "BongCoder",
-  description: "봉우리 코더 (봉우 리코더 - 봉우라는 사람의 리코더라는 뜻이  아닙니다.",
+  description:
+    "봉우리 코더 (봉우 리코더 - 봉우라는 사람의 리코더라는 뜻이  아닙니다.",
 };
 
 export default function RootLayout({
@@ -17,9 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body>
-        <CompNavbar/>
+      <body className="min-h-screen ">
+        <nav>
+          <CompNavbar />
+        </nav>
+
         {children}
+        <footer className="border-t-2 my-auto">
+          <p>Footer</p>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-export default function HomePage(){
-  return(<>
-  <h1>
-    홈페이지입니다.
-  </h1>
-  </>)
+export default function HomePage() {
+  return (
+    <>
+      <h1>홈페이지입니다.</h1>
+      <p>안에 들어갈 내용을 적으세요.</p>
+    </>
+  );
 }
