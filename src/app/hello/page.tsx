15b58c8693fac10370b2097196f937ea899aa626
퇴역군인 <freebird_han@naver.com>
2024-07-11 10:29:01 +0900

    feat: Improve code readability and add variable explanations

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,33 +1,43 @@
 import Introduce from "@/components/hello/Introduce";
 
 export default function HelloPage() {
-  const name: string = "봉붕이";
+
+  // 변수라는 걸 알아보자.
+  // 변수 =  메모리에 어떤 값을 나중에 쓰려고 저장하는거야. 
+  // 방법은
+  // const   -> 상수 == 안바뀜
+  // let -> 바뀜 ㄱㅊ
+  // var -> 이거는 안쓰는게 좋아.
+
+let 이연희 = 1;
+// 데이터 타입
+// typescript / javascript 라는거에는 
+// 데이터가 
+// string
+// number 
+// boolean
+// object
+// array
+// null
+  
+
+
+  const name: string = "연희쨩";
   const age: number = 345;
   const introduce: string[] = [
     "안녕, 나는 봉붕이야.",
-    "나는 345살이야.",
+    "나는 345살이야.",//선생님 쓸래쉬 2개 넣으면 주석맞죠?
     "나는 봉우리 코더야.",
   ];
+  // js -> 동작, 기능
+  // css 
+  // html -> 구조/ 내용
   return (
     <>
-      <h1 className="text-center font-black text-xl">봉우리코더자기소개</h1>
+    <p>
 
-      <article className="border-2 p-2 rounded-md ">
-      <h2>저는 이런 사람 입니다.</h2>
-        <p>
-          <span className="font-bold">이름:</span>
-          {name}
-        </p>
-        <p>나이: {age}</p>
-        <div className="border-2 p-2 rounded-md">
-          <h3 className="text-center font-bold">자기소개</h3>
-          {introduce.map((a: string, i: number) => {
-            return <p key={i}>{a}</p>;
-          })}
-        </div>
-          <Introduce/>
-
-      </article>
+      {name}
+    </p>
     </>
   );
 }
